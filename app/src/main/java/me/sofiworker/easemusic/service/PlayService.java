c4c203699af267a6400435d2a3111be3ef80d949
sofiworker <sofiworker@outlook.com>
2019-12-05 00:21:28 +0800

    编写部分播放服务

--- app/src/main/java/me/sofiworker/easemusic/service/PlayService.java
+++ app/src/main/java/me/sofiworker/easemusic/service/PlayService.java
@@ -3,9 +3,17 @@ package me.sofiworker.easemusic.service;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.media.MediaBrowserCompat;
 import android.support.v4.media.MediaBrowserServiceCompat;
+import android.support.v4.media.MediaDescriptionCompat;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.text.TextUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,20 +24,97 @@ import java.util.List;
  */
 public class PlayService extends MediaBrowserServiceCompat {
 
+    private static final String MEDIA_SESSION_TAG = "media_session_tag";
+    private static final String MEDIA_ROOT_ID = "media_root_id";
+    private static final String EMPTY_MEDIA_ROOT_ID = "empty_root_id";
+
+    private MediaSessionCompat mMediaSession;
+    private PlaybackStateCompat.Builder mStateBuilder;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // Create a MediaSessionCompat
+        mMediaSession = new MediaSessionCompat(getApplicationContext(), MEDIA_SESSION_TAG);
+
+        // Enable callbacks from MediaButtons and TransportControls
+        mMediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        // Set an initial PlaybackState with ACTION_PLAY, so media buttons can start the player
+        mStateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+
+        // MySessionCallback() has methods that handle callbacks from a media controller
+        mMediaSession.setCallback(new MediaSessionCallback());
+
+        // Set the session's token so that client activities can communicate with it.
+        setSessionToken(mMediaSession.getSessionToken());
     }
 
+    /**
+     * 控制对服务的访问
+     */
     @Nullable
     @Override
-    public BrowserRoot onGetRoot(@NonNull String s, int i, @Nullable Bundle bundle) {
-        return null;
+    public BrowserRoot onGetRoot(@NonNull String clientPackageName, int clientUid,
+                                 @Nullable Bundle bundle) {
+        // (Optional) Control the level of access for the specified package name.
+        // You'll need to write your own logic to do this.
+        if (allowBrowsing(clientPackageName, clientUid)) {
+            // Returns a root ID that clients can use with onLoadChildren() to retrieve
+            // the content hierarchy.
+            return new BrowserRoot(MEDIA_ROOT_ID, null);
+        } else {
+            // Clients can connect, but this BrowserRoot is an empty hierachy
+            // so onLoadChildren returns nothing. This disables the ability to browse for content.
+            return new BrowserRoot(EMPTY_MEDIA_ROOT_ID, null);
+        }
     }
 
+    private boolean allowBrowsing(String clientPackageName, int clientUid) {
+        return false;
+    }
+
+    /**
+     * 为客户端提供构建和显示MediaBrowserService的内容层次结构菜单的能力
+     */
     @Override
-    public void onLoadChildren(@NonNull String s, @NonNull Result<List<MediaBrowserCompat.MediaItem>> result) {
+    public void onLoadChildren(@NonNull String parentMediaId, @NonNull Result<List<MediaBrowserCompat.MediaItem>> result) {
+        //  Browsing not allowed
+        if (TextUtils.equals(EMPTY_MEDIA_ROOT_ID, parentMediaId)) {
+            result.sendResult(null);
+            return;
+        }
+
+        // Assume for example that the music catalog is already loaded/cached.
+
+        List<MediaBrowserCompat.MediaItem> mediaItems = new ArrayList<>();
+
+        // Check if this is the root menu:
+        if (MEDIA_ROOT_ID.equals(parentMediaId)) {
+            // Build the MediaItem objects for the top level,
+            // and put them in the mediaItems list...
+        } else {
+            // Examine the passed parentMediaId to see which submenu we're at,
+            // and put the children of that menu in the mediaItems list...
+        }
+        result.sendResult(mediaItems);
+    }
+
+    public class MediaSessionCallback extends MediaSessionCompat.Callback {
+        // Given a media session and its context (usually the component containing the session)
+        // Create a NotificationCompat.Builder
+        // Get the session's metadata
+        MediaControllerCompat controller = mMediaSession.getController();
+        MediaMetadataCompat mediaMetadata = controller.getMetadata();
+        MediaDescriptionCompat description = mediaMetadata.getDescription();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
 
     }
 }
